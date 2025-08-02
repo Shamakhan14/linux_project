7ce50936c8dda8f8b69215ebd3868286acea33fd
Valentin Usenko <valentin-u-a@yandex.ru>
2025-08-02 17:29:52 +0300

    fixed folder name for nginx script

--- nginx/nginx_script.sh
+++ nginx/nginx_script.sh
@@ -6,7 +6,7 @@ APACHE1_IP=$1
 OLD_APACHE1_IP="old_apache1_ip"
 APACHE2_IP=$2
 OLD_APACHE2_IP="old_apache2_ip"
-NGINX_CONF_NAME="sites-available/default"
+NGINX_CONF_NAME="nginx_config/sites-available/default"
 
 sed -i "s/$OLD_APACHE1_IP/$APACHE1_IP/g" "$NGINX_CONF_NAME"
 sed -i "s/$OLD_APACHE2_IP/$APACHE2_IP/g" "$NGINX_CONF_NAME"
