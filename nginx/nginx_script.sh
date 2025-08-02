bc20c42ec6e14b15934258f8112384304d2e5677
Valentin Usenko <valentin-u-a@yandex.ru>
2025-08-02 17:25:07 +0300

    added nginx script

--- nginx/nginx_script.sh
+++ nginx/nginx_script.sh
@@ -2,4 +2,18 @@
 
 #command: sh network_setup.sh {apache1_ip} {apache2_ip}
 
-cat
+APACHE1_IP=$1
+OLD_APACHE1_IP="old_apache1_ip"
+APACHE2_IP=$2
+OLD_APACHE2_IP="old_apache2_ip"
+NGINX_CONF_NAME="sites-available/default"
+
+sed -i "s/$OLD_APACHE1_IP/$APACHE1_IP/g" "$NGINX_CONF_NAME"
+sed -i "s/$OLD_APACHE2_IP/$APACHE2_IP/g" "$NGINX_CONF_NAME"
+
+cp -r nginx_config/* /etc/nginx/
+cp -r icons/* /usr/share/apache2/icons/
+cp -r html/* /var/www/html
+
+service nginx reload
+service nginx restart
