fb11b50fbd654fe90b252001f3627b40723d718f
Valentin Usenko <valentin-u-a@yandex.ru>
2025-09-24 19:07:13 +0300

    added instruction file

--- nginx/nginx_script.sh
+++ nginx/nginx_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#command: sh network_setup.sh {apache1_ip} {apache2_ip}
+#command: sh nginx_scipt.sh {apache1_ip} {apache2_ip}
 #apache1_ip: 192.168.31.73
 #apache2_ip: 192.168.31.20
 
